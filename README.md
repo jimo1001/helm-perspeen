bae70bdb98805ac6b87c48a997c6c680ca0a4ba0
Yoshinobu Fujimoto <jimo1001@gmail.com>
2017-02-03 01:44:40 +0900

    Add tabs of Helm sources.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This package provides **simple manageability** of using **perspeen** workspaces with **Helm** interface.
 
+## Installaton / Configuration
+
+Using `use-package`
+``` emacs-lisp
+(use-package helm-perspeen :defer t)
+```
--- helm-perspeen.el
+++ helm-perspeen.el
@@ -1,12 +1,13 @@
 ;;; helm-perspeen.el --- helm extension for perspeen.el
+
 ;; Copyright (C) 2017 jimo1001 <jimo1001@gmail.com>
 ;;
 ;; Author: Yoshinobu Fujimoto
-;; URL:https://github.com/jimo1001/helm-perspeen
+;; URL: https://github.com/jimo1001/helm-perspeen
 ;; Created: 2017-01-30
 ;; Version: 0.1.0
 ;; Keywords: projects, lisp
-;; Package0Requires: (("emacs" "25.0") ("perspeen" "0.1.0"))
+;; Package-Requires: ((perspeen "0.1.0") (helm-projectile "2.5.1"))
 
 ;;  URLs
 ;;  - perspeen
@@ -16,7 +17,6 @@
 ;;  - helm-projectile
 ;;    - https://github.com/bbatsov/helm-projectile
 ;;
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -29,30 +29,55 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-;;
 
 ;;; Commentary:
 
 ;; This package provides helm interface for perspeen's workspaces.
+;;
+;; Command:
+;;   - M-x helm-perspeen
+;; Helm Sources:
+;;   - helm-source-perspeen-tabs
+;;   - helm-source-perspeen-workspaces
+;;   - helm-source-perspeen-create-workspace
+;;
 
 ;;; Code:
+
+
 
 (require 'helm)
 (require 'perspeen)
 (require 'helm-projectile)
 
-(defvar helm-source-perspeen
+
+(defvar helm-source-perspeen-tabs
+  (helm-build-sync-source "Tabs (perspeen)"
+    :candidates
+    (lambda ()
+      (if perspeen-tab-configurations
+          (let ((index -1) (buffer nil))
+            (mapcar (lambda (tab)
+                      (setq index (+ index 1))
+                      (setq buffer (get tab 'current-buffer))
+                      (cons (format "%s" (buffer-name buffer)) index))
+                    (perspeen-tab-get-tabs)))
+        nil))
+    :action
+    (lambda (index) (perspeen-tab-switch-internal index) nil))
+  "The helm source which are perspeen's tabs in the current workspace.")
+
+(defvar helm-source-perspeen-workspaces
   (helm-build-sync-source "WorkSpaces (perspeen)"
     :candidates
     (lambda ()
-      (let ((index 0))
-        (mapcar (lambda (ws)
-                  (setq index (+ index 1))
-                  (list (format "%s (%s)" (elt ws 1) (elt ws 5)) index))
-                perspeen-ws-list)))
+      (mapcar (lambda (ws)
+                (let ((name (perspeen-ws-struct-name ws))
+                      (root-dir (perspeen-ws-struct-root-dir ws)))
+                  (cons (format "%s (%s)" name root-dir) ws)))
+              perspeen-ws-list))
     :action
-    (lambda (index)
-      (apply 'perspeen-goto-ws index)))
+    'perspeen-switch-ws-internal)
   "The workspaces helm source for perspeen.el.")
 
 (defun helm-perspeen-create-projectile-workspace (dir)
@@ -62,7 +87,7 @@ DIR is project root directory."
   (perspeen-create-ws)
   (perspeen-change-root-dir dir))
 
-(defvar helm-source-create-workspace
+(defvar helm-source-perspeen-create-workspace
   (helm-build-dummy-source
       "Create perspeen workspace"
     :action (helm-make-actions
@@ -82,7 +107,7 @@ DIR is project root directory."
 (defun helm-perspeen ()
   "Display workspaces (perspeen.el) with helm interface."
   (interactive
-   (helm '(helm-source-perspeen helm-source-create-workspace))))
+   (helm '(helm-source-perspeen-tabs helm-source-perspeen-workspaces helm-source-perspeen-create-workspace))))
 
 (provide 'helm-perspeen)
 
